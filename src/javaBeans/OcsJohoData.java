9b02e161eb9ecad53ec09b30e01c326118d1db35
ocsLoveberry <ocsloveberry@gmail.com>
2018-12-19 14:57:29 +0900

    fix:ocsjohobeansの修正

--- src/javaBeans/OcsJohoData.java
+++ src/javaBeans/OcsJohoData.java
@@ -10,23 +10,23 @@ public class OcsJohoData {
 
 	}
 
-	public OcsJohoData(String name,String seki_no,String comment){
-		setName(name);
+	public OcsJohoData(String seki_no,String name,String comment){
 		setSeki_no(seki_no);
+		setName(name);
 		setComment(comment);
 	}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public String getSeki_no() {
 		return seki_no;
 	}
 	public void setSeki_no(String seki_no) {
 		this.seki_no = seki_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	public String getComment() {
 		return comment;
