4da45ca023a9a902c6bc215124e8438bbc856893
kesera2 <ocsloveberry@gmail.com>
2019-01-31 11:27:25 +0900

    fix:daobase修正の影響箇所を直しました

--- src/main/dao/Search_teacherDAO.java
+++ src/main/dao/Search_teacherDAO.java
@@ -54,8 +54,6 @@ public class Search_teacherDAO extends DAOBase{
 				}
 		} catch (SQLException e) {
 			throw new DatabaseException(ExceptionParameters.DATABASE_CONNECTION_EXCEPTION_MASSAGE, e);
-		} catch (SystemException e) {
-			e.printStackTrace();
 		} finally {
 			this.close(stmt);
 		}
